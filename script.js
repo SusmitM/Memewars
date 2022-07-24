54f2333dc32d50b6e8f5189da1a3b683931d19c7
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-07-24 17:07:02 +0530

    download location changed

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 let btnDownload1 = document.getElementById("downld-btn1");
 let btnDownload2 = document.getElementById("downld-btn2");
-let img1 = document.querySelector('meme');
+let likeBtn1 = document.getElementById("like-btn1");
+let likeBtn2 = document.getElementById("like-btn2");
+let img1 = document.querySelector("meme");
 let img2 = document.querySelector("meme1");
 
+
+
 $.getJSON("https://meme-api.herokuapp.com/gimme", function (data) {
-    
+
 
     var meme = data.preview[1];
 
     $(".meme").attr("src", meme);
 
     btnDownload1.addEventListener('click', () => {
-        let imagePath = data.preview[1];
+        let imagePath = data.url;
         let fileName = "memeFromMemeWars";
         saveAs(imagePath, fileName);
     });
-     
-     
-  
+
+
+
 
 
 });
@@ -30,14 +34,17 @@ $.getJSON("https://meme-api.herokuapp.com/gimme/2", function (data) {
     $(".meme1").attr("src", meme1);
 
     btnDownload2.addEventListener('click', () => {
-        let imagePath = data.memes[0].preview[1];
+        let imagePath = data.memes[0].url;
         let fileName = "memeFromMemeWars";
         saveAs(imagePath, fileName);
     });
+    
 });
 
 
-
-
-
-
+likeBtn1.addEventListener('click', () => {
+    window.location.reload();
+});
+likeBtn2.addEventListener('click', () => {
+    window.location.reload();
+});
