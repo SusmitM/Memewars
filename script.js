a103df85c5f6498ff707bb3878833babf7e5b553
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-11-26 01:23:07 +0530

    filesaver script changed

--- script.js
+++ script.js
@@ -6,50 +6,56 @@ let img1 = document.querySelector("meme");
 let img2 = document.querySelector("meme1");
 
 
-let url1="https://meme-api.herokuapp.com/gimme";
-let url2="https://meme-api.herokuapp.com/gimme/2";
+let url1 = "https://meme-api.herokuapp.com/gimme";
+let url2 = "https://meme-api.herokuapp.com/gimme/2";
+
 
 // For the first meme Box
 fetch(url1)
-.then(response=> response.json())
-.then((data)=>{
-    console.log(data);
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
 
-    var meme = data.preview[1];
-
-
-// To show the meme
-$(".meme").attr("src", meme);
+        var meme = data.preview[1];
 
 
-  //To download the meme
-  btnDownload1.addEventListener('click', () => {
+        // To show the meme
+        $(".meme").attr("src", meme);
+
+
+        //To download the meme
+
+        btnDownload1.addEventListener('click', () => {
+
             let imagePath = data.url;
-           let fileName = "memeFromMemeWars.jpg";
+            let fileName = "memeFromMemeWars.jpg";
             saveAs(imagePath, fileName);
         });
-});
+
+    });
+
 
 // For the second meme Box
 fetch(url2)
-.then(response=> response.json())
-.then((data)=>{
-    console.log(data);
-    var meme1 = data.memes[0].preview[1];
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
+        var meme1 = data.memes[0].preview[1];
 
 
-// To show the meme
-  $(".meme1").attr("src", meme1);
+        // To show the meme
+        $(".meme1").attr("src", meme1);
 
 
-  //To download the meme
-  btnDownload2.addEventListener('click', () => {
+        //To download the meme
+        btnDownload2.addEventListener('click', () => {
             let imagePath = data.memes[0].url;
             let fileName = "memeFromMemeWars.jpg";
-           saveAs(imagePath, fileName);
-       });
+            
+             saveAs(imagePath, fileName);
+        });
 
-});
+    });
 
 
 
