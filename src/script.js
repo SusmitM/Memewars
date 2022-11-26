16bcf56691ffeefabdc641068e4201f872c7e188
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-11-26 20:23:08 +0530

    testing the new download function wrt to CORS

--- src/script.js
+++ src/script.js
@@ -10,6 +10,29 @@ let url1 = "https://meme-api.herokuapp.com/gimme";
 let url2 = "https://meme-api.herokuapp.com/gimme/2";
 
 
+
+
+function forceDownload(link){
+    var url = link.getAttribute("data-href");
+    var fileName = link.getAttribute("download");
+   
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function(){
+        var urlCreator = window.URL || window.webkitURL;
+        var imageUrl = urlCreator.createObjectURL(this.response);
+        var tag = document.createElement('a');
+        tag.href = imageUrl;
+        tag.download = fileName;
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+    }
+    xhr.send();
+}
+
+
 // For the first meme Box
 fetch(url1)
     .then(response => response.json())
@@ -25,12 +48,15 @@ fetch(url1)
 
         //To download the meme
 
-        btnDownload1.addEventListener('click', () => {
+        // btnDownload1.addEventListener('click', (e) => {
 
-            let imagePath = data.url;
-            let fileName = "memeFromMemeWars.jpg";
-            saveAs(imagePath, fileName);
-        });
+        //     let imagePath = data.url;
+        //     // let fileName = "memeFromMemeWars.jpg";
+        //     // saveAs(imagePath, fileName);
+        //     e.preventDefault();
+
+        //   window.location.href = imagePath;
+        // });
 
     });
 
@@ -48,12 +74,21 @@ fetch(url2)
 
 
         //To download the meme
-        btnDownload2.addEventListener('click', () => {
-            let imagePath = data.memes[0].url;
-            let fileName = "memeFromMemeWars.jpg";
+        // btnDownload2.addEventListener('click', () => {
+        //     // let imagePath = data.memes[0].url;
+        //     // let fileName = "memeFromMemeWars.jpg";
             
-             saveAs(imagePath, fileName);
-        });
+        //     //  saveAs(imagePath, fileName);
+        //     fetch(url2)
+        // .then(res => res.blob())
+        // .then((blob) => {
+        //     console.log(blob);
+        //     saveAs(blob, 'my-file-label.jpg');
+        // })
+
+
+      
+        // });
 
     });
 
